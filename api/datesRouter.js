0997e5e0415c7f93738f46f201c58ce817db0fcb
Mohab <mohabisback@yahoo.com>
2022-08-22 23:35:03 +0200

    adjustment

--- api/datesRouter.js
+++ api/datesRouter.js
@@ -1,19 +1,31 @@
 import express from 'express'
-
 const router = express.Router()
+
+router.route('/').get(async (req, res, next)=>{
+  try {    
+    let date = new Date()
+    let str = date.toString()
+    let format = str.slice(0,3) + ', '+ str.slice(8,10) + ' ' + str.slice(4,7) + ' ' + str.slice(11, 28)
+    res.status(200).json({unix: date.getTime(), utc: format})
+  } catch (error) {
+    console.log(error)
+  }
+})
+
 router.route('/:date').get(async (req, res, next)=>{
   try {
-    console.log('i got here')
-    let dateStr = req.params.date
-    let date = new Date(dateStr)
+    let date = new Date(req.params.date)
+    let str = date.toString();
+    let format = str.slice(0,3) + ', '+ str.slice(8,10) + ' ' + str.slice(4,7) + ' ' + str.slice(11, 28)
+
     if (date.getTime()){
-      res.status(200).json({unix: date.getTime(), utc: date.toString()})
+      res.status(200).json({unix: date.getTime(), utc: format})
     } else {
-      res.status(404).json({error: 'not found'})
+      res.status(404).json({error: 'Invalid Date'}) 
     }
   } catch (error) {
     console.log(error)
-    res.status(404).json({error: 'not found'})
+    res.status(404).json({error: 'Invalid Date'})
   }
 })
 
